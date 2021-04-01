2112b1f650a2369635f14252ff48587bc8191034
Naman Jain <namanb139j@gmail.com>
2021-04-02 00:15:57 +0530

    ff

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
   )*/
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+  
   //res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin')
 
   if (req.method === 'OPTIONS') {
