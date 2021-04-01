d7f0ee89e19cba487eb1e8c2f9e52e396259d212
Naman Jain <namanb139j@gmail.com>
2021-04-02 00:28:36 +0530

    dfdgdgd

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ app.use((req, res, next) => {
     'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   )*/
   res.header("Access-Control-Allow-Origin", 'https://codeplaynr.netlify.app');
-  //res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   
   //res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin')
 
