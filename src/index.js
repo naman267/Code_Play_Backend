9256de7ff772caf65e0240540fd67d36d864016d
Naman Jain <namanb139j@gmail.com>
2022-12-25 17:49:08 +0530

    hh

--- src/index.js
+++ src/index.js
@@ -20,14 +20,14 @@ app.use((req, res, next) => {
   //console.log(req)
   
   console.log('CORS middleware')
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   )
-  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, PATCH, DELETE')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, POST, PATCH, DELETE')
 
-  res.header('Cross-Origin-Resource-Policy', 'cross-origin')
+  res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin')
   if (req.method === 'OPTIONS') {
  
     
